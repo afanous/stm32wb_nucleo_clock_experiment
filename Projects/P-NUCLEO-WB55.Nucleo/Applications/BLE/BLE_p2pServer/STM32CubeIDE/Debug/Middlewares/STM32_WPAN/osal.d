ff5880183ef4fb768f5a8bf5e5613c5a2dddd9c0
Tony Fanous <afanous1985@gmail.com>
2026-06-14 17:55:07 -0700

    Add files

---
+++ Projects/P-NUCLEO-WB55.Nucleo/Applications/BLE/BLE_p2pServer/STM32CubeIDE/Debug/Middlewares/STM32_WPAN/osal.d
@@ -0,0 +1,4 @@
+Middlewares/STM32_WPAN/osal.o: \
+ C:/Users/tony_/STM32Cube/Repository/STM32Cube_FW_WB_V1.22.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
+ C:/Users/tony_/STM32Cube/Repository/STM32Cube_FW_WB_V1.22.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+C:/Users/tony_/STM32Cube/Repository/STM32Cube_FW_WB_V1.22.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
